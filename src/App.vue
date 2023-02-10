9f1d56b720f9ce009b51442fcd680b858f44717b
Giovanni Aceto <55949483+GioAceto@users.noreply.github.com>
2023-02-10 10:40:14 -0500

    update

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <todo-app />
+</template>
+
+<script setup>
+import TodoApp from "./components/TodoApp.vue";
+</script>
+
+<style scoped>
+.todo-app {
+  margin: 0 auto;
+  padding: 5vh;
+  min-height: 100vh;
+  width: 500px;
+  background-color: #f7f9fc;
+}
+</style>
